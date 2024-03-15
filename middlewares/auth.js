81f2a752f081380e3bafff145d75f3942142c624
akshu2626 <akshugajbhiye97@gmail.com>
2024-03-15 22:25:01 +0530

    all

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,16 +3,19 @@ const ErrorHandler = require("../utils/ErrorHandler");
 const { catchAsyncErrors } = require("./catchAsyncError");
 
 exports.isAuthenticated = catchAsyncErrors(async (req, res, next) => {
-    const { token } = req.headers.authorization;
+    const authorizationHeader = req.headers.authorization;
 
-    if (!token) {
-        return next(new ErrorHandler("please login to access the resources", 401))
+    if (!authorizationHeader || !authorizationHeader.startsWith("Bearer")) {
+        return next(new ErrorHandler("Please provide a valid token", 401));
     }
-    const { id } = jwt.verify(token, process.env.JWT_SECRET);
 
-    req.id = id;
+    const token = authorizationHeader.split(" ")[1];
 
-    // res.status(200).json({status:true,token:token})
-
-    next()
-})
+    try {
+        const { id } = jwt.verify(token, process.env.JWT_SECRET);
+        req.id = id;
+        next();
+    } catch (error) {
+        return next(new ErrorHandler("Invalid or expired token", 401));
+    }
+});
