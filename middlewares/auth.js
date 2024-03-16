1848acdae0a67e5f0da2a9c9e343ab2cb5ed0670
akshu2626 <akshugajbhiye97@gmail.com>
2024-03-16 23:44:21 +0530

    add some line

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const ErrorHandler = require("../utils/ErrorHandler");
 const { catchAsyncErrors } = require("./catchAsyncError");
 exports.isAuthenticated = catchAsyncErrors(async (req, res, next) => {
-    const { token } = req.cookies.jwtoken;
+    const { token } = req.cookies;
     if (!token) {
         return next(new ErrorHandler("please login to access the resources", 401))
     }
