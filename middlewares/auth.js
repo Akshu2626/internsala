4c7068e0485ffffc4994f2740822c9bde027f8eb
akshu2626 <akshugajbhiye97@gmail.com>
2024-03-17 01:24:41 +0530

    abcd

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -15,7 +15,7 @@ function verifyToken(req, res, next) {
             const token = tokenParts[1];
 
             // Token verify karo
-            jwt.verify(token, 'your_secret_key', (err, decoded) => {
+            jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
                 if (err) {
                     return res.status(401).json({ message: 'Unauthorized' });
                 } else {
@@ -35,17 +35,6 @@ function verifyToken(req, res, next) {
 }
 
 module.exports = verifyToken;
-
-
-
-
-
-
-
-
-
-
-
 
 // exports.isAuthenticated = catchAsyncErrors(async (req, res, next) => {
 //     const { token } = req.headers['authorization'];
