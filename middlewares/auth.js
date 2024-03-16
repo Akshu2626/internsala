64030b0fc1f45f3a09199cc4cba4994351733585
akshu2626 <akshugajbhiye97@gmail.com>
2024-03-17 01:21:32 +0530

    abb

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,15 +1,61 @@
-const jwt = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
 const ErrorHandler = require("../utils/ErrorHandler");
 const { catchAsyncErrors } = require("./catchAsyncError");
-exports.isAuthenticated = catchAsyncErrors(async (req, res, next) => {
-    const { token } = req.headers['authorization'];
-    if (!token) {
-        return next(new ErrorHandler("please login to access the resources", 401))
+
+const jwt = require('jsonwebtoken');
+
+function verifyToken(req, res, next) {
+    const authorizationHeader = req.headers['authorization'];
+
+    if (authorizationHeader) {
+        // Token ko split karo
+        const tokenParts = authorizationHeader.split(' ');
+
+        if (tokenParts.length === 2 && tokenParts[0] === 'Bearer') {
+            const token = tokenParts[1];
+
+            // Token verify karo
+            jwt.verify(token, 'your_secret_key', (err, decoded) => {
+                if (err) {
+                    return res.status(401).json({ message: 'Unauthorized' });
+                } else {
+                    // Token verified, decoded information ko request object me add karo
+                    req.user = decoded;
+                    next();
+                }
+            });
+        } else {
+            // Invalid token format
+            return res.status(401).json({ message: 'Unauthorized' });
+        }
+    } else {
+        // Authorization header missing
+        return res.status(401).json({ message: 'Unauthorized' });
     }
-    const { id } = jwt.verify(token, req.id, process.env.JWT_SECRET);
+}
 
-    req.id = id;
+module.exports = verifyToken;
 
 
-    next()
-})
+
+
+
+
+
+
+
+
+
+
+// exports.isAuthenticated = catchAsyncErrors(async (req, res, next) => {
+//     const { token } = req.headers['authorization'];
+//     if (!token) {
+//         return next(new ErrorHandler("please login to access the resources", 401))
+//     }
+//     const { id } = jwt.verify(token, req.id, process.env.JWT_SECRET);
+
+//     req.id = id;
+
+
+//     next()
+// })
