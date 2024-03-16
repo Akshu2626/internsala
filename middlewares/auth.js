8539b11d0502350cc5435f53316da6154f5fb80d
akshu2626 <akshugajbhiye97@gmail.com>
2024-03-17 01:28:33 +0530

    abcbedf

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,50 +1,23 @@
-// const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 const ErrorHandler = require("../utils/ErrorHandler");
 const { catchAsyncErrors } = require("./catchAsyncError");
 
-const jwt = require('jsonwebtoken');
-
-function verifyToken(req, res, next) {
+exports.isAuthenticated = catchAsyncErrors(async (req, res, next) => {
     const authorizationHeader = req.headers['authorization'];
 
-    if (authorizationHeader) {
-        // Token ko split karo
-        const tokenParts = authorizationHeader.split(' ');
-
-        if (tokenParts.length === 2 && tokenParts[0] === 'Bearer') {
-            const token = tokenParts[1];
-
-            // Token verify karo
-            jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-                if (err) {
-                    return res.status(401).json({ message: 'Unauthorized' });
-                } else {
-                    // Token verified, decoded information ko request object me add karo
-                    req.user = decoded;
-                    next();
-                }
-            });
-        } else {
-            // Invalid token format
-            return res.status(401).json({ message: 'Unauthorized' });
-        }
-    } else {
-        // Authorization header missing
-        return res.status(401).json({ message: 'Unauthorized' });
+    if (!authorizationHeader || !authorizationHeader.startsWith('Bearer')) {
+        return next(new ErrorHandler("Please provide a valid token in Authorization header", 401));
     }
-}
 
-module.exports = verifyToken;
+    const token = authorizationHeader.split(' ')[1];
 
-// exports.isAuthenticated = catchAsyncErrors(async (req, res, next) => {
-//     const { token } = req.headers['authorization'];
-//     if (!token) {
-//         return next(new ErrorHandler("please login to access the resources", 401))
-//     }
-//     const { id } = jwt.verify(token, req.id, process.env.JWT_SECRET);
-
-//     req.id = id;
-
-
-//     next()
-// })
+    try {
+        // JWT verify ke liye secret key ki zarurat hai, isliye aapko JWT_SECRET ko use karna chahiye
+        const { id } = jwt.verify(token, process.env.JWT_SECRET);
+        req.id = id;
+        next();
+    } catch (error) {
+        // JWT verify fail hone par, unauthorized error throw karo
+        return next(new ErrorHandler("Invalid or expired token", 401));
+    }
+});
